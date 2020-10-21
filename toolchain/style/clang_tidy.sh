546a362c8401dbc1df0b0fdf6c3db1a650de6c97
Leland Jansen <ljansen@microsoft.com>
2020-10-21 11:52:57 -0700

    Fix formatting (#25)

--- toolchain/style/clang_tidy.sh
+++ toolchain/style/clang_tidy.sh
@@ -4,10 +4,10 @@ set -e
 
 WORKSPACE="$(bazel info workspace)"
 
-if command -v clang-tidy &> /dev/null; then
-  CLANG_TIDY="clang-tidy"
-elif command -v clang-tidy-11 &> /dev/null; then
+if command -v clang-tidy-11 &> /dev/null; then
   CLANG_TIDY="clang-tidy-11"
+elif command -v clang-tidy &> /dev/null; then
+  CLANG_TIDY="clang-tidy"
 else
   echo "clang-tidy not found"
   exit 1
