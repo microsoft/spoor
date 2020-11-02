02af818447f9428114163f11faf9651e0c443d61
Leland Jansen <ljansen@microsoft.com>
2020-11-01 23:09:48 -0800

    Amalgamated buffer slice pool

--- spoor/runtime/buffer/amalgamated_buffer_slice_pool.h
+++ spoor/runtime/buffer/amalgamated_buffer_slice_pool.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <utility>
+#include <vector>
 
 #include "spoor/runtime/buffer/buffer_slice_pool.h"
 #include "spoor/runtime/buffer/circular_buffer.h"
@@ -71,5 +72,122 @@ class AmalgamatedBufferSlicePool final : public BufferSlicePool<T> {
   ReservedPool reserved_pool_;
   DynamicPool dynamic_pool_;
 };
+
+template <class T>
+constexpr AmalgamatedBufferSlicePool<T>::AmalgamatedBufferSlicePool(
+    const Options& options)
+    : options_{options},
+      reserved_pool_{ReservedPool{options.reserved_pool_options}},
+      dynamic_pool_{DynamicPool{options.dynamic_pool_options}} {}
+
+template <class T>
+constexpr auto AmalgamatedBufferSlicePool<T>::Borrow(
+    const SizeType preferred_slice_capacity) -> BorrowResult {
+  auto reserved_pool_slice_result =
+      reserved_pool_.Borrow(preferred_slice_capacity);
+  if (reserved_pool_slice_result.IsOk()) {
+    return std::move(reserved_pool_slice_result);
+  }
+  auto dynamic_pool_slice_result =
+      dynamic_pool_.Borrow(preferred_slice_capacity);
+  return std::move(dynamic_pool_slice_result);
+}
+
+template <class T>
+constexpr auto AmalgamatedBufferSlicePool<T>::Return(OwnedSlicePtr&& slice)
+    -> ReturnResult {
+  auto owner = slice.Owner();
+  if (owner != &reserved_pool_ && owner != &dynamic_pool_) {
+    return std::move(slice);
+  }
+  auto result = owner->Return(std::move(slice));
+  if (result.IsOk()) return ReturnResult::Ok({});
+  return std::move(result.Err());
+}
+
+template <class T>
+template <class Collection>
+constexpr auto AmalgamatedBufferSlicePool<T>::Return(Collection&& slices)
+    -> std::vector<OwnedSlicePtr> {
+  std::vector<OwnedSlicePtr> slices_owned_by_other_pools{};
+  for (auto& slice : slices) {
+    auto result = Return(std::move(slice));
+    if (result.IsErr()) {
+      slices_owned_by_other_pools.push_back(std::move(result.Err()));
+    }
+  }
+  return slices_owned_by_other_pools;
+}
+
+template <class T>
+constexpr auto AmalgamatedBufferSlicePool<T>::ReservedPoolSize() const
+    -> SizeType {
+  return reserved_pool_.Size();
+}
+
+template <class T>
+constexpr auto AmalgamatedBufferSlicePool<T>::DynamicPoolSize() const
+    -> SizeType {
+  return dynamic_pool_.Size();
+}
+
+template <class T>
+constexpr auto AmalgamatedBufferSlicePool<T>::Size() const -> SizeType {
+  return ReservedPoolSize() + DynamicPoolSize();
+}
+
+template <class T>
+constexpr auto AmalgamatedBufferSlicePool<T>::ReservedPoolCapacity() const
+    -> SizeType {
+  return reserved_pool_.Capacity();
+}
+
+template <class T>
+constexpr auto AmalgamatedBufferSlicePool<T>::DynamicPoolCapacity() const
+    -> SizeType {
+  return dynamic_pool_.Capacity();
+}
+
+template <class T>
+constexpr auto AmalgamatedBufferSlicePool<T>::Capacity() const -> SizeType {
+  return ReservedPoolCapacity() + DynamicPoolCapacity();
+}
+
+template <class T>
+constexpr auto AmalgamatedBufferSlicePool<T>::ReservedPoolEmpty() const
+    -> bool {
+  return reserved_pool_.Empty();
+}
+
+template <class T>
+constexpr auto AmalgamatedBufferSlicePool<T>::DynamicPoolEmpty() const -> bool {
+  return dynamic_pool_.Empty();
+}
+
+template <class T>
+constexpr auto AmalgamatedBufferSlicePool<T>::Empty() const -> bool {
+  return ReservedPoolEmpty() && DynamicPoolEmpty();
+}
+
+template <class T>
+constexpr auto AmalgamatedBufferSlicePool<T>::ReservedPoolFull() const -> bool {
+  return reserved_pool_.Full();
+}
+
+template <class T>
+constexpr auto AmalgamatedBufferSlicePool<T>::DynamicPoolFull() const -> bool {
+  return dynamic_pool_.Full();
+}
+
+template <class T>
+constexpr auto AmalgamatedBufferSlicePool<T>::Full() const -> bool {
+  return ReservedPoolFull() && DynamicPoolFull();
+}
+
+template <class T>
+constexpr auto AmalgamatedBufferSlicePool<T>::Return(Slice* /*unused*/)
+    -> ReturnRawPtrResult {
+  return ReturnRawPtrResult::Err({});
+}
 
 }  // namespace spoor::runtime::buffer
