2bbec1187ccb0bb47d6abec07359f8314108b313
Leland Jansen <ljansen@microsoft.com>
2020-11-10 12:37:45 -0800

    Circular slice buffer (#32)

---
+++ spoor/runtime/buffer/circular_buffer_test.cc
@@ -0,0 +1,214 @@
+#include "spoor/runtime/buffer/circular_buffer.h"
+
+#include <algorithm>
+#include <iterator>
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "gsl/gsl"
+#include "gtest/gtest.h"
+#include "spoor/runtime/buffer/circular_slice_buffer.h"
+#include "spoor/runtime/buffer/owned_buffer_slice.h"
+#include "spoor/runtime/buffer/reserved_buffer_slice_pool.h"
+#include "spoor/runtime/buffer/unowned_buffer_slice.h"
+#include "util/numeric.h"
+
+namespace {
+
+using CircularBuffer = spoor::runtime::buffer::CircularBuffer<int64>;
+using ValueType = CircularBuffer::ValueType;
+using SizeType = CircularBuffer::SizeType;
+using Pool = spoor::runtime::buffer::ReservedBufferSlicePool<ValueType>;
+using OwnedSlicePtr = util::memory::OwnedPtr<CircularBuffer>;
+
+auto MakePool(const SizeType capacity) -> std::unique_ptr<Pool> {
+  const typename Pool::Options options{
+      .max_slice_capacity = std::max(static_cast<SizeType>(1), capacity / 2),
+      .capacity = capacity};
+  return std::make_unique<Pool>(options);
+}
+
+template <class T>
+auto MakeBuffers(
+    gsl::span<T> buffer,
+    gsl::not_null<spoor::runtime::buffer::ReservedBufferSlicePool<T>*> pool)
+    -> std::vector<std::unique_ptr<spoor::runtime::buffer::CircularBuffer<T>>> {
+  using CircularBuffer = spoor::runtime::buffer::CircularBuffer<T>;
+  using OwnedBufferSlice = spoor::runtime::buffer::OwnedBufferSlice<T>;
+  using UnownedBufferSlice = spoor::runtime::buffer::UnownedBufferSlice<T>;
+  using CircularSliceBuffer = spoor::runtime::buffer::CircularSliceBuffer<T>;
+  std::vector<std::unique_ptr<CircularBuffer>> circular_buffers{};
+  circular_buffers.reserve(3);
+  circular_buffers.push_back(std::make_unique<UnownedBufferSlice>(buffer));
+  circular_buffers.push_back(std::make_unique<OwnedBufferSlice>(buffer.size()));
+  const typename CircularSliceBuffer::Options circular_slice_buffer_options{
+      .buffer_slice_pool = pool, .capacity = pool->Capacity()};
+  circular_buffers.push_back(
+      std::make_unique<CircularSliceBuffer>(circular_slice_buffer_options));
+  return circular_buffers;
+}
+
+TEST(CircularBuffer, PushCopyValue) {  // NOLINT
+  using Pool = spoor::runtime::buffer::ReservedBufferSlicePool<std::string>;
+  const auto assert_chunks_equal_to_expected =
+      [](const std::vector<gsl::span<Pool::ValueType>>& chunks,
+         const Pool::SizeType capacity, const int64 start_value) {
+        ASSERT_EQ(chunks.size(), 1);
+        std::vector<int64> expected_chunk_numbers(capacity);
+        std::iota(std::begin(expected_chunk_numbers),
+                  std::end(expected_chunk_numbers), start_value);
+        std::vector<std::string> expected_chunk{};
+        expected_chunk.reserve(expected_chunk_numbers.size());
+        std::transform(std::cbegin(expected_chunk_numbers),
+                       std::cend(expected_chunk_numbers),
+                       std::back_inserter(expected_chunk),
+                       [](const auto i) { return std::to_string(i); });
+        const auto& chunk = chunks.front();
+        ASSERT_EQ(chunk.size(), expected_chunk.size());
+        ASSERT_TRUE(std::equal(std::cbegin(chunk), std::cend(chunk),
+                               std::cbegin(expected_chunk)));
+      };
+  for (const SizeType capacity : {1, 2, 10}) {
+    std::vector<std::string> buffer(capacity);
+    const typename Pool::Options options{.max_slice_capacity = capacity,
+                                         .capacity = capacity};
+    auto pool = std::make_unique<Pool>(options);
+    for (auto& circular_buffer : MakeBuffers<std::string>(buffer, pool.get())) {
+      for (SizeType i{0}; i < capacity; ++i) {
+        const auto value = std::to_string(i);
+        circular_buffer->Push(value);
+      }
+      ASSERT_TRUE(circular_buffer->Full());
+      const auto chunks = circular_buffer->ContiguousMemoryChunks();
+      assert_chunks_equal_to_expected(chunks, capacity, 0);
+
+      for (SizeType i{capacity}; i < 2 * capacity; ++i) {
+        const auto value = std::to_string(i);
+        circular_buffer->Push(value);
+      }
+      ASSERT_TRUE(circular_buffer->Full());
+      const auto wrapped_chunks = circular_buffer->ContiguousMemoryChunks();
+      assert_chunks_equal_to_expected(wrapped_chunks, capacity, capacity);
+    }
+  }
+}
+
+TEST(CircularBuffer, PushMoveValue) {  // NOLINT
+  const auto assert_chunks_equal_to_expected =
+      [](const std::vector<gsl::span<ValueType>>& chunks,
+         const Pool::SizeType capacity, const ValueType start_value) {
+        ASSERT_EQ(chunks.size(), 1);
+        std::vector<int64> expected_chunk(capacity);
+        std::iota(std::begin(expected_chunk), std::end(expected_chunk),
+                  start_value);
+        const auto& chunk = chunks.front();
+        ASSERT_EQ(chunk.size(), expected_chunk.size());
+        ASSERT_TRUE(std::equal(std::cbegin(chunk), std::cend(chunk),
+                               std::cbegin(expected_chunk)));
+      };
+  for (const SizeType capacity : {1, 2, 10}) {
+    std::vector<ValueType> buffer(capacity);
+    const typename Pool::Options options{.max_slice_capacity = capacity,
+                                         .capacity = capacity};
+    auto pool = std::make_unique<Pool>(options);
+    for (auto& circular_buffer : MakeBuffers<ValueType>(buffer, pool.get())) {
+      for (SizeType i{0}; i < capacity; ++i) {
+        circular_buffer->Push(i);
+      }
+      ASSERT_TRUE(circular_buffer->Full());
+      const auto chunks = circular_buffer->ContiguousMemoryChunks();
+      assert_chunks_equal_to_expected(chunks, capacity, 0);
+
+      for (SizeType i{capacity}; i < 2 * capacity; ++i) {
+        circular_buffer->Push(i);
+      }
+      ASSERT_TRUE(circular_buffer->Full());
+      const auto wrapped_chunks = circular_buffer->ContiguousMemoryChunks();
+      assert_chunks_equal_to_expected(wrapped_chunks, capacity, capacity);
+    }
+  }
+}
+
+TEST(CircularBuffer, Clear) {  // NOLINT
+  for (const SizeType capacity : {0, 1, 2, 10}) {
+    std::vector<ValueType> buffer(capacity);
+    auto pool = MakePool(capacity);
+    for (auto& circular_buffer : MakeBuffers<ValueType>(buffer, pool.get())) {
+      for (SizeType i{0}; i < capacity; ++i) {
+        circular_buffer->Push(i);
+      }
+      circular_buffer->Clear();
+      ASSERT_EQ(circular_buffer->Size(), 0);
+    }
+  }
+}
+
+TEST(CircularBuffer, Size) {  // NOLINT
+  for (const SizeType capacity : {0, 1, 2, 10}) {
+    std::vector<ValueType> buffer(capacity);
+    auto pool = MakePool(capacity);
+    for (auto& circular_buffer : MakeBuffers<ValueType>(buffer, pool.get())) {
+      for (SizeType i{0}; i < 2 * capacity; ++i) {
+        circular_buffer->Push(i);
+        ASSERT_EQ(circular_buffer->Size(), std::min(i + 1, capacity));
+      }
+    }
+  }
+}
+
+TEST(CircularBuffer, Capacity) {  // NOLINT
+  for (const SizeType capacity : {0, 1, 2, 10}) {
+    std::vector<ValueType> buffer(capacity);
+    auto pool = MakePool(capacity);
+    for (auto& circular_buffer : MakeBuffers<ValueType>(buffer, pool.get())) {
+      for (SizeType i{0}; i < 2 * capacity; ++i) {
+        circular_buffer->Push(i);
+        ASSERT_EQ(circular_buffer->Capacity(), capacity);
+      }
+    }
+  }
+}
+
+TEST(CircularBuffer, Empty) {  // NOLINT
+  for (const SizeType capacity : {0, 1, 2, 10}) {
+    std::vector<ValueType> buffer(capacity);
+    auto pool = MakePool(capacity);
+    for (auto& circular_buffer : MakeBuffers<ValueType>(buffer, pool.get())) {
+      ASSERT_TRUE(circular_buffer->Empty());
+      for (SizeType i{0}; i < capacity; ++i) {
+        circular_buffer->Push(i);
+        ASSERT_FALSE(circular_buffer->Empty());
+      }
+    }
+  }
+}
+
+TEST(CircularBuffer, WillWrapOnNextPush) {  // NOLINT
+  for (const SizeType capacity : {0, 1, 2, 10}) {
+    std::vector<ValueType> buffer(capacity);
+    auto pool = MakePool(capacity);
+    for (auto& circular_buffer : MakeBuffers<ValueType>(buffer, pool.get())) {
+      for (SizeType i{0}; i < 5 * capacity + 5; ++i) {
+        if (capacity == 0 || (i != 0 && i % capacity == 0)) {
+          ASSERT_TRUE(circular_buffer->WillWrapOnNextPush());
+        } else {
+          ASSERT_FALSE(circular_buffer->WillWrapOnNextPush());
+        }
+        circular_buffer->Push(i);
+      }
+    }
+  }
+}
+
+TEST(CircularBuffer, ContiguousMemoryChunksEmpty) {  // NOLINT
+  constexpr SizeType capacity{0};
+  std::vector<ValueType> buffer(capacity);
+  auto pool = MakePool(capacity);
+  for (auto& circular_buffer : MakeBuffers<ValueType>(buffer, pool.get())) {
+    ASSERT_TRUE(circular_buffer->ContiguousMemoryChunks().empty());
+  }
+}
+
+}  // namespace
---
+++ spoor/runtime/buffer/circular_slice_buffer_test.cc
@@ -0,0 +1,116 @@
+#include "spoor/runtime/buffer/circular_slice_buffer.h"
+
+#include <algorithm>
+#include <initializer_list>
+
+#include "gmock/gmock.h"
+#include "gsl/gsl"
+#include "gtest/gtest.h"
+#include "spoor/runtime/buffer/reserved_buffer_slice_pool.h"
+#include "util/numeric.h"
+
+namespace {
+
+using SlicePool = spoor::runtime::buffer::BufferSlicePool<uint64>;
+using CircularBuffer = spoor::runtime::buffer::CircularBuffer<int64>;
+using ValueType = CircularBuffer::ValueType;
+using SizeType = CircularBuffer::SizeType;
+using CircularSliceBuffer =
+    spoor::runtime::buffer::CircularSliceBuffer<ValueType>;
+using Pool = spoor::runtime::buffer::ReservedBufferSlicePool<ValueType>;
+using OwnedSlicePtr = util::memory::OwnedPtr<CircularBuffer>;
+
+constexpr SizeType kCapacity{10};
+
+auto operator==(const std::vector<gsl::span<ValueType>>& result_chunks,
+                const std::vector<std::vector<ValueType>>& expected_chunks)
+    -> bool {
+  if (std::size(result_chunks) != std::size(expected_chunks)) return false;
+  for (SizeType index{0}; index < std::size(expected_chunks); ++index) {
+    const auto result_chunk = result_chunks.at(index);
+    const auto expected_chunk = expected_chunks.at(index);
+    const auto equal =
+        std::equal(std::cbegin(result_chunk), std::cend(result_chunk),
+                   std::cbegin(expected_chunk));
+    if (!equal) return false;
+  }
+  return true;
+}
+
+TEST(CircularSliceBuffer, ContiguousMemoryChunks) {  // NOLINT
+  const typename Pool::Options options{.max_slice_capacity = 2,
+                                       .capacity = kCapacity};
+  Pool pool{options};
+  const typename CircularSliceBuffer::Options circular_slice_buffer_options{
+      .buffer_slice_pool = &pool, .capacity = kCapacity};
+  CircularSliceBuffer circular_slice_buffer{circular_slice_buffer_options};
+
+  ASSERT_TRUE(circular_slice_buffer.Empty());
+  ASSERT_TRUE(circular_slice_buffer.ContiguousMemoryChunks().empty());
+  ASSERT_TRUE(operator==(circular_slice_buffer.ContiguousMemoryChunks(), {}));
+  circular_slice_buffer.Push(0);
+  ASSERT_TRUE(operator==
+              (circular_slice_buffer.ContiguousMemoryChunks(), {{0}}));
+  circular_slice_buffer.Push(1);
+  ASSERT_TRUE(operator==
+              (circular_slice_buffer.ContiguousMemoryChunks(), {{0, 1}}));
+  circular_slice_buffer.Push(2);
+  ASSERT_TRUE(operator==
+              (circular_slice_buffer.ContiguousMemoryChunks(), {{0, 1}, {2}}));
+  for (SizeType i{3}; i < kCapacity; ++i) {
+    circular_slice_buffer.Push(i);
+  }
+  ASSERT_TRUE(operator==(circular_slice_buffer.ContiguousMemoryChunks(),
+                         {{0, 1}, {2, 3}, {4, 5}, {6, 7}, {8, 9}}));
+  circular_slice_buffer.Push(10);
+  ASSERT_TRUE(operator==(circular_slice_buffer.ContiguousMemoryChunks(),
+                         {{1}, {2, 3}, {4, 5}, {6, 7}, {8, 9}, {10}}));
+  circular_slice_buffer.Push(11);
+  ASSERT_TRUE(operator==(circular_slice_buffer.ContiguousMemoryChunks(),
+                         {{2, 3}, {4, 5}, {6, 7}, {8, 9}, {10, 11}}));
+  for (SizeType i{12}; i < 2 * kCapacity; ++i) {
+    circular_slice_buffer.Push(i);
+  }
+  ASSERT_TRUE(operator==(circular_slice_buffer.ContiguousMemoryChunks(),
+                         {{10, 11}, {12, 13}, {14, 15}, {16, 17}, {18, 19}}));
+}
+
+TEST(CircularSliceBuffer, PushWrapsIfBorrowFails) {  // NOLINT
+  const typename Pool::Options options{.max_slice_capacity = 1,
+                                       .capacity = kCapacity};
+  Pool pool{options};
+  const typename CircularSliceBuffer::Options circular_slice_buffer_options{
+      .buffer_slice_pool = &pool,
+      .capacity = kCapacity,
+  };
+  CircularSliceBuffer circular_slice_buffer_a{circular_slice_buffer_options};
+  ASSERT_TRUE(circular_slice_buffer_a.Empty());
+  for (SizeType i{0}; i < kCapacity; ++i) {
+    circular_slice_buffer_a.Push(i);
+  }
+  ASSERT_TRUE(operator==(circular_slice_buffer_a.ContiguousMemoryChunks(),
+                         {{0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}}));
+
+  CircularSliceBuffer circular_slice_buffer_b{circular_slice_buffer_options};
+  ASSERT_TRUE(circular_slice_buffer_b.Empty());
+  for (SizeType i{0}; i < kCapacity; ++i) {
+    circular_slice_buffer_a.Push(i);
+    ASSERT_TRUE(circular_slice_buffer_b.Empty());
+  }
+
+  circular_slice_buffer_a.Clear();
+  ASSERT_TRUE(circular_slice_buffer_a.Empty());
+
+  for (SizeType i{0}; i < kCapacity / 2; ++i) {
+    circular_slice_buffer_a.Push(i);
+  }
+  ASSERT_TRUE(operator==(circular_slice_buffer_a.ContiguousMemoryChunks(),
+                         {{0}, {1}, {2}, {3}, {4}}));
+  for (SizeType i{0}; i < kCapacity; ++i) {
+    circular_slice_buffer_b.Push(i);
+  }
+  ASSERT_TRUE(operator==(circular_slice_buffer_b.ContiguousMemoryChunks(),
+                         {{5}, {6}, {7}, {8}, {9}}));
+}
+
+}  // namespace
