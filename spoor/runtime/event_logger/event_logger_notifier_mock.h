8fc81fe49173e4e8cc4142cab12800fb778a56b4
Leland Jansen <ljansen@microsoft.com>
2020-11-16 02:12:46 -0800

    Event logger (#43)

---
+++ spoor/runtime/event_logger/event_logger_notifier_mock.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "gmock/gmock.h"
+#include "spoor/runtime/event_logger/event_logger_notifier.h"
+
+namespace spoor::runtime::event_logger::testing {
+
+class EventLoggerNotifierMock final : public EventLoggerNotifier {
+ public:
+  MOCK_METHOD(void, Subscribe, (gsl::not_null<EventLogger*> subscriber),
+              (override));
+  MOCK_METHOD(void, Unsubscribe, (gsl::not_null<EventLogger*> subscriber),
+              (override));
+};
+
+}  // namespace spoor::runtime::event_logger::testing
