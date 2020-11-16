0a41de6a46b64a6eeebd61b96b31dae9232948ed
Leland Jansen <ljansen@microsoft.com>
2020-11-16 02:31:14 -0800

    Env util

---
+++ util/env/env.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <functional>
+#include <string>
+#include <type_traits>
+
+#include "absl/strings/numbers.h"
+
+namespace util::env {
+
+using GetEnv = std::function<const char*(const char*)>;
+
+auto GetEnvOrDefault(const char* key, std::string default_value,
+                     const GetEnv& get_env) -> std::string;
+
+auto GetEnvOrDefault(const char* key, bool default_value, const GetEnv& get_env)
+    -> bool;
+
+template <class T, class = std::enable_if_t<std::is_integral_v<T> &&
+                                            !std::is_same_v<T, bool>>>
+auto GetEnvOrDefault(const char* key, const T default_value,
+                     const GetEnv& get_env) -> T {
+  const auto* user_value = get_env(key);
+  if (user_value == nullptr) return default_value;
+  T value{};
+  const auto success = absl::SimpleAtoi(user_value, &value);
+  return success ? value : default_value;
+}
+
+}  // namespace util::env
