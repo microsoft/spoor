d2b7fca9bd8d2f633f057f796c2f222dd1ca1eeb
Leland Jansen <ljansen@microsoft.com>
2020-11-16 14:38:29 -0800

    Runtime config (#45)

---
+++ spoor/runtime/config/config.h
@@ -0,0 +1,84 @@
+#pragma once
+
+#include <cstdlib>
+#include <filesystem>
+#include <limits>
+#include <random>
+#include <string_view>
+
+#include "spoor/runtime/buffer/circular_buffer.h"
+#include "spoor/runtime/trace/trace.h"
+#include "util/env/env.h"
+
+namespace spoor::runtime::config {
+
+constexpr std::string_view kTraceFilePathKey{"SPOOR_RUNTIME_TRACE_FILE_PATH"};
+// NOLINTNEXTLINE(fuchsia-statically-constructed-objects)
+const std::string kTraceFilePathDefaultValue{};
+constexpr std::string_view kSessionIdKey{"SPOOR_RUNTIME_SESSION_ID"};
+// NOLINTNEXTLINE(fuchsia-statically-constructed-objects)
+constexpr auto kSessionIdDefaultValue = [] {
+  std::random_device seed{};
+  std::default_random_engine engine{seed()};
+  std::uniform_int_distribution<trace::SessionId> distribution{};
+  return distribution(engine);
+};
+constexpr std::string_view kThreadEventBufferCapacityKey{
+    "SPOOR_RUNTIME_THEAD_EVENT_BUFFER_CAPACITY"};
+constexpr buffer::CircularBuffer<trace::Event>::SizeType
+    kThreadEventBufferCapacityDefaultValue{10'000};
+constexpr std::string_view kMaxReservedEventBufferSliceCapacityKey{
+    "SPOOR_RUNTIME_MAX_RESERVED_EVENT_BUFFER_SLICE_CAPACITY"};
+constexpr buffer::CircularBuffer<trace::Event>::SizeType
+    kMaxReservedEventBufferSliceCapacityDefaultValue{1'000};
+constexpr std::string_view kMaxDynamicEventBufferSliceCapacityKey{
+    "SPOOR_RUNTIME_MAX_DYNAMIC_EVENT_BUFFER_SLICE_CAPACITY"};
+constexpr buffer::CircularBuffer<trace::Event>::SizeType
+    kMaxDynamicEventBufferSliceCapacityDefaultValue{1'000};
+constexpr std::string_view kReservedEventPoolCapacityKey{
+    "SPOOR_RUNTIME_RESERVED_EVENT_POOL_CAPACITY"};
+constexpr buffer::CircularBuffer<trace::Event>::SizeType
+    kReservedEventPoolCapacityDefaultValue{0};
+constexpr std::string_view kDynamicEventPoolCapacityKey{
+    "SPOOR_RUNTIME_DYNAMIC_EVENT_POOL_CAPACITY"};
+constexpr buffer::CircularBuffer<
+    trace::Event>::SizeType kDynamicEventPoolCapacityDefaultValue{
+    std::numeric_limits<buffer::CircularBuffer<trace::Event>::SizeType>::max()};
+constexpr std::string_view kDynamicEventSliceBorrowCasAttemptsKey{
+    "SPOOR_RUNTIME_DYNAMIC_EVENT_SLICE_BORROW_CAS_ATTEMPTS"};
+constexpr buffer::CircularBuffer<trace::Event>::SizeType
+    kDynamicEventSliceBorrowCasAttemptsDefaultValue{1};
+constexpr std::string_view kEventBufferRetentionDurationNanosecondsKey{
+    "SPOOR_RUNTIME_EVENT_BUFFER_RETENTION_DURATION_NANOSECONDS"};
+constexpr trace::DurationNanoseconds
+    kEventBufferRetentionNanosecondsDefaultValue{0};
+constexpr std::string_view kMaxFlushBufferToFileAttemptsKey{
+    "SPOOR_RUNTIME_MAX_FLUSH_BUFFER_TO_FILE_ATTEMPTS"};
+constexpr int32 kMaxFlushBufferToFileAttemptsDefaultValue{
+    std::numeric_limits<int32>::max()};
+constexpr std::string_view kFlushAllEventsKey{"SPOOR_RUNTIME_FLUSH_ALL_EVENTS"};
+constexpr bool kFlushAllEventsDefaultValue{true};
+
+struct Config {
+  using SizeType = buffer::CircularBuffer<trace::Event>::SizeType;
+
+  static auto FromEnv(const util::env::GetEnv& get_env = [](const char* key) {
+    return std::getenv(key);
+  }) -> Config;
+
+  std::filesystem::path trace_file_path;
+  trace::SessionId session_id;
+  SizeType thread_event_buffer_capacity;
+  SizeType max_reserved_event_buffer_slice_capacity;
+  SizeType max_dynamic_event_buffer_slice_capacity;
+  SizeType reserved_event_pool_capacity;
+  SizeType dynamic_event_pool_capacity;
+  SizeType dynamic_event_slice_borrow_cas_attempts;
+  trace::DurationNanoseconds event_buffer_retention_duration_nanoseconds;
+  int32 max_flush_buffer_to_file_attempts;
+  bool flush_all_events;
+};
+
+auto operator==(const Config& lhs, const Config& rhs) -> bool;
+
+};  // namespace spoor::runtime::config
---
+++ spoor/runtime/config/config_test.cc
@@ -0,0 +1,80 @@
+#include "spoor/runtime/config/config.h"
+
+#include <limits>
+#include <string_view>
+#include <unordered_map>
+
+#include "gtest/gtest.h"
+#include "spoor/runtime/buffer/circular_buffer.h"
+
+namespace {
+
+using spoor::runtime::config::Config;
+using spoor::runtime::config::kDynamicEventPoolCapacityKey;
+using spoor::runtime::config::kDynamicEventSliceBorrowCasAttemptsKey;
+using spoor::runtime::config::kEventBufferRetentionDurationNanosecondsKey;
+using spoor::runtime::config::kFlushAllEventsKey;
+using spoor::runtime::config::kMaxDynamicEventBufferSliceCapacityKey;
+using spoor::runtime::config::kMaxFlushBufferToFileAttemptsKey;
+using spoor::runtime::config::kMaxReservedEventBufferSliceCapacityKey;
+using spoor::runtime::config::kReservedEventPoolCapacityKey;
+using spoor::runtime::config::kSessionIdKey;
+using spoor::runtime::config::kThreadEventBufferCapacityKey;
+using spoor::runtime::config::kTraceFilePathKey;
+using SizeType = spoor::runtime::buffer::CircularBuffer<
+    spoor::runtime::trace::Event>::SizeType;
+
+TEST(Config, GetsUserProvidedValue) {  // NOLINT
+  const auto get_env = [](const char* key) {
+    const std::unordered_map<std::string_view, std::string_view> environment{
+        {kTraceFilePathKey, "/path/to/file.extension"},
+        {kSessionIdKey, "42"},
+        {kThreadEventBufferCapacityKey, "42"},
+        {kMaxReservedEventBufferSliceCapacityKey, "42"},
+        {kMaxDynamicEventBufferSliceCapacityKey, "42"},
+        {kReservedEventPoolCapacityKey, "42"},
+        {kDynamicEventPoolCapacityKey, "42"},
+        {kDynamicEventSliceBorrowCasAttemptsKey, "42"},
+        {kEventBufferRetentionDurationNanosecondsKey, "42"},
+        {kMaxFlushBufferToFileAttemptsKey, "42"},
+        {kFlushAllEventsKey, "false"}};
+    return environment.at(key).data();
+  };
+  const Config expected_options{
+      .trace_file_path = "/path/to/file.extension",
+      .session_id = 42,
+      .thread_event_buffer_capacity = 42,
+      .max_reserved_event_buffer_slice_capacity = 42,
+      .max_dynamic_event_buffer_slice_capacity = 42,
+      .reserved_event_pool_capacity = 42,
+      .dynamic_event_pool_capacity = 42,
+      .dynamic_event_slice_borrow_cas_attempts = 42,
+      .event_buffer_retention_duration_nanoseconds = 42,
+      .max_flush_buffer_to_file_attempts = 42,
+      .flush_all_events = false};
+  ASSERT_EQ(Config::FromEnv(get_env), expected_options);
+}
+
+TEST(Config, UsesDefaultValueWhenNotSpecified) {  // NOLINT
+  const auto get_env = [](const char* /*unused*/) -> const char* {
+    return nullptr;
+  };
+  Config expected_options{
+      .trace_file_path = "",
+      .session_id = 0,  // Ignored
+      .thread_event_buffer_capacity = 10'000,
+      .max_reserved_event_buffer_slice_capacity = 1'000,
+      .max_dynamic_event_buffer_slice_capacity = 1'000,
+      .reserved_event_pool_capacity = 0,
+      .dynamic_event_pool_capacity = std::numeric_limits<SizeType>::max(),
+      .dynamic_event_slice_borrow_cas_attempts = 1,
+      .event_buffer_retention_duration_nanoseconds = 0,
+      .max_flush_buffer_to_file_attempts = std::numeric_limits<int32>::max(),
+      .flush_all_events = true};
+  const auto options = Config::FromEnv(get_env);
+  // Ignore `session_id` which is randomly generated.
+  expected_options.session_id = options.session_id;
+  ASSERT_EQ(options, expected_options);
+}
+
+}  // namespace
