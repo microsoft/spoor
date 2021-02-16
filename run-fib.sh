0703963e35a08ef8cbaff5fab3c73ca34d0af026
Tommy Meisel <thmeisel@microsoft.com>
2021-02-16 15:21:20 -0800

    remove python from WORKSPACE

--- run-fib.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-bazel run //spoor/tools:main -- cat "/Users/lelandjansen/Desktop/MyAmazingApp/**/*.spoor_function_map" "/Users/lelandjansen/Desktop/MyAmazingApp/trace/*.spoor_trace" > ~/Desktop/fib_trace.json
--- run-outlook.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-bazel run //spoor/tools:main -- cat "/Users/lelandjansen/projects/client-cocoa/**/*.spoor_function_map" "/Users/lelandjansen/projects/client-cocoa/trace/*.spoor_trace" > /Users/lelandjansen/Desktop/outlook_trace.json
