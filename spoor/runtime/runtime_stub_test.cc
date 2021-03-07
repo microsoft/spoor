f40cc51d7837259db47a42c7fccffa7c8d81ba62
Leland Jansen <ljansen@microsoft.com>
2021-03-06 22:56:25 -0800

    Runtime stub (#98)

--- spoor/runtime/runtime_stub_test.cc
+++ spoor/runtime/runtime_stub_test.cc
@@ -1,64 +1,45 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-#include "spoor/runtime/runtime.h"
-
 #include <future>
 #include <limits>
 
 #include "gsl/gsl"
 #include "gtest/gtest.h"
+#include "spoor/runtime/runtime.h"
 
 namespace {
 
 TEST(Runtime, Initialize) {  // NOLINT
-  for (auto iteration{0}; iteration < 3; ++iteration) {
-    ASSERT_FALSE(_spoor_runtime_RuntimeEnabled());
-    ASSERT_FALSE(_spoor_runtime_RuntimeInitialized());
-    _spoor_runtime_InitializeRuntime();
-    ASSERT_FALSE(_spoor_runtime_RuntimeEnabled());
-    ASSERT_TRUE(_spoor_runtime_RuntimeInitialized());
-    _spoor_runtime_InitializeRuntime();
-    ASSERT_FALSE(_spoor_runtime_RuntimeEnabled());
-    ASSERT_TRUE(_spoor_runtime_RuntimeInitialized());
-    _spoor_runtime_DeinitializeRuntime();
-    ASSERT_FALSE(_spoor_runtime_RuntimeEnabled());
-    ASSERT_FALSE(_spoor_runtime_RuntimeInitialized());
-    _spoor_runtime_DeinitializeRuntime();
-    ASSERT_FALSE(_spoor_runtime_RuntimeEnabled());
-    ASSERT_FALSE(_spoor_runtime_RuntimeInitialized());
-  }
+  ASSERT_FALSE(_spoor_runtime_RuntimeInitialized());
+  _spoor_runtime_InitializeRuntime();
+  ASSERT_FALSE(_spoor_runtime_RuntimeInitialized());
+  _spoor_runtime_DeinitializeRuntime();
+  ASSERT_FALSE(_spoor_runtime_RuntimeInitialized());
 }
 
 TEST(Runtime, Enable) {  // NOLINT
   ASSERT_FALSE(_spoor_runtime_RuntimeEnabled());
-  _spoor_runtime_InitializeRuntime();
-  ASSERT_FALSE(_spoor_runtime_RuntimeEnabled());
-  for (auto iteration{0}; iteration < 3; ++iteration) {
-    _spoor_runtime_EnableRuntime();
-    ASSERT_TRUE(_spoor_runtime_RuntimeEnabled());
-    _spoor_runtime_EnableRuntime();
-    ASSERT_TRUE(_spoor_runtime_RuntimeEnabled());
-    _spoor_runtime_DisableRuntime();
-    ASSERT_FALSE(_spoor_runtime_RuntimeEnabled());
-    _spoor_runtime_DisableRuntime();
-    ASSERT_FALSE(_spoor_runtime_RuntimeEnabled());
-  }
   _spoor_runtime_EnableRuntime();
-  ASSERT_TRUE(_spoor_runtime_RuntimeEnabled());
-  _spoor_runtime_DeinitializeRuntime();
+  ASSERT_FALSE(_spoor_runtime_RuntimeEnabled());
+  _spoor_runtime_DisableRuntime();
   ASSERT_FALSE(_spoor_runtime_RuntimeEnabled());
 }
+
+namespace flush_trace_events_test {
+
+// clang-format off NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables, fuchsia-statically-constructed-objects) clang-format on
+std::promise<void> promise_{};
 
 TEST(Runtime, FlushTraceEvents) {  // NOLINT
   _spoor_runtime_FlushTraceEvents({});
-  _spoor_runtime_InitializeRuntime();
-  _spoor_runtime_FlushTraceEvents({});
+  _spoor_runtime_FlushTraceEvents([] { promise_.set_value(); });
+  promise_.get_future();
 }
 
+}  // namespace flush_trace_events_test
+
 TEST(Runtime, ClearTraceEvents) {  // NOLINT
-  _spoor_runtime_ClearTraceEvents();
-  _spoor_runtime_InitializeRuntime();
   _spoor_runtime_ClearTraceEvents();
 }
 
@@ -68,6 +49,7 @@ namespace flused_trace_files_test {
 std::promise<_spoor_runtime_TraceFiles> promise_{};
 
 TEST(Runtime, FlushedTraceFiles) {  // NOLINT
+  _spoor_runtime_FlushedTraceFiles({});
   _spoor_runtime_FlushedTraceFiles(
       [](auto trace_files) { promise_.set_value(trace_files); });
   auto future = promise_.get_future();
@@ -86,7 +68,9 @@ std::promise<_spoor_runtime_DeletedFilesInfo> promise_{};
 
 TEST(Runtime, DeleteFlushedTraceFilesOlderThan) {  // NOLINT
   _spoor_runtime_DeleteFlushedTraceFilesOlderThan(
-      std::numeric_limits<_spoor_runtime_SystemTimestampSeconds>::max(),
+      std::numeric_limits<_spoor_runtime_SystemTimestampSeconds>::min(), {});
+  _spoor_runtime_DeleteFlushedTraceFilesOlderThan(
+      std::numeric_limits<_spoor_runtime_SystemTimestampSeconds>::min(),
       [](auto deleted_files_info) { promise_.set_value(deleted_files_info); });
   auto future = promise_.get_future();
   const auto deletes_files_info = future.get();
@@ -98,11 +82,21 @@ TEST(Runtime, DeleteFlushedTraceFilesOlderThan) {  // NOLINT
 
 TEST(Runtime, GetConfig) {  // NOLINT
   const auto config = _spoor_runtime_GetConfig();
-  ASSERT_NE(config.trace_file_path, nullptr);
+  ASSERT_EQ(config.trace_file_path, nullptr);
+  ASSERT_EQ(config.session_id, 0);
+  ASSERT_EQ(config.thread_event_buffer_capacity, 0);
+  ASSERT_EQ(config.max_reserved_event_buffer_slice_capacity, 0);
+  ASSERT_EQ(config.max_dynamic_event_buffer_slice_capacity, 0);
+  ASSERT_EQ(config.reserved_event_pool_capacity, 0);
+  ASSERT_EQ(config.dynamic_event_pool_capacity, 0);
+  ASSERT_EQ(config.dynamic_event_slice_borrow_cas_attempts, 0);
+  ASSERT_EQ(config.event_buffer_retention_duration_nanoseconds, 0);
+  ASSERT_EQ(config.max_flush_buffer_to_file_attempts, 0);
+  ASSERT_FALSE(config.flush_all_events);
 }
 
 TEST(Runtime, StubImplementation) {  // NOLINT
-  ASSERT_FALSE(_spoor_runtime_StubImplementation());
+  ASSERT_TRUE(_spoor_runtime_StubImplementation());
 }
 
 }  // namespace
