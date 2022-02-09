1485ee7ada80a8bbbce3fb5f84180b66c7e99848
Leland Jansen <ljansen@microsoft.com>
2022-02-09 11:41:12 -0800

    Remove Runtime C API (#224)

--- spoor/runtime/runtime_common_test.cc
+++ spoor/runtime/runtime_common_test.cc
@@ -4,122 +4,27 @@
 #include <array>
 #include <string>
 
-#include "gsl/gsl"
 #include "gtest/gtest.h"
 #include "spoor/runtime/runtime.h"
 
 namespace {
 
-TEST(Runtime, ReleaseTraceFilePaths) {  // NOLINT
-  constexpr _spoor_runtime_TraceFiles empty_trace_files{
-      .file_paths_size = 0, .file_path_sizes = nullptr, .file_paths = nullptr};
-  constexpr auto path_a_size{5};
-  constexpr auto path_b_size{10};
-  constexpr auto paths_size{2};
-  // NOLINTNEXTLINE(cppcoreguidelines-no-malloc)
-  gsl::span<char> path_a{static_cast<char*>(malloc(sizeof(char) * path_a_size)),
-                         path_a_size};
-  // NOLINTNEXTLINE(cppcoreguidelines-no-malloc)
-  gsl::span<char> path_b{static_cast<char*>(malloc(sizeof(char) * path_b_size)),
-                         path_b_size};
-  gsl::span<_spoor_runtime_SizeType> path_sizes{
-      static_cast<_spoor_runtime_SizeType*>(
-          // NOLINTNEXTLINE(cppcoreguidelines-no-malloc)
-          malloc(sizeof(_spoor_runtime_SizeType) * paths_size)),
-      paths_size};
-  gsl::span<char*> paths{
-      // NOLINTNEXTLINE(cppcoreguidelines-no-malloc)
-      static_cast<char**>(malloc(sizeof(char*) * paths_size)), paths_size};
-  paths[0] = path_a.data();
-  paths[1] = path_b.data();
-  _spoor_runtime_TraceFiles trace_files{.file_paths_size = 2,
-                                        .file_path_sizes = path_sizes.data(),
-                                        .file_paths = paths.data()};
-  ASSERT_NE(trace_files, empty_trace_files);
-  _spoor_runtime_ReleaseTraceFilePaths(&trace_files);
-  ASSERT_EQ(trace_files, empty_trace_files);
-  _spoor_runtime_ReleaseTraceFilePaths(&trace_files);
-  ASSERT_EQ(trace_files, empty_trace_files);
-}
-
-TEST(Runtime, TraceFilesEquality) {  // NOLINT
-  std::string path_a{"/path/a"};
-  std::string path_b{"/path/b"};
-  std::string path_c{"/path/to/c"};
-  std::string path_d{};
-  std::string path_e{"/path/a"};
-  ASSERT_EQ(path_a, path_e);
-  std::array<_spoor_runtime_SizeType, 4> file_path_sizes_a{
-      {path_a.size(), path_b.size(), path_c.size(), path_d.size()}};
-  std::array<char*, 4> file_paths_a{
-      {path_a.data(), path_b.data(), path_c.data(), path_d.data()}};
-  ASSERT_NE(file_paths_a.size(), 0);
-  ASSERT_EQ(file_path_sizes_a.size(), file_paths_a.size());
-  std::array<_spoor_runtime_SizeType, 1> file_path_sizes_b{{path_e.size()}};
-  std::array<char*, 1> file_paths_b{{path_e.data()}};
-  _spoor_runtime_TraceFiles trace_files_a{
-      .file_paths_size = file_paths_a.size(),
-      .file_path_sizes = file_path_sizes_a.data(),
-      .file_paths = file_paths_a.data()};
-  _spoor_runtime_TraceFiles trace_files_b{
-      .file_paths_size = file_paths_a.size(),
-      .file_path_sizes = file_path_sizes_a.data(),
-      .file_paths = file_paths_a.data()};
-  _spoor_runtime_TraceFiles trace_files_c{
-      .file_paths_size = 1,
-      .file_path_sizes = file_path_sizes_a.data(),
-      .file_paths = file_paths_a.data()};
-  _spoor_runtime_TraceFiles trace_files_d{
-      .file_paths_size = file_paths_b.size(),
-      .file_path_sizes = file_path_sizes_b.data(),
-      .file_paths = file_paths_b.data()};
-  _spoor_runtime_TraceFiles malformed_trace_files_e{
-      .file_paths_size = file_paths_a.size(),
-      .file_path_sizes = nullptr,
-      .file_paths = nullptr};
-  _spoor_runtime_TraceFiles malformed_trace_files_f{
-      .file_paths_size = file_paths_a.size(),
-      .file_path_sizes = nullptr,
-      .file_paths = file_paths_a.data()};
-  // Note: ASAN will fail when testing a struct whose `file_paths_size` is
-  // greater than the size of `file_paths_size` or `file_paths`.
-  ASSERT_TRUE(_spoor_runtime_TraceFilesEqual(nullptr, nullptr));
-  ASSERT_FALSE(_spoor_runtime_TraceFilesEqual(&trace_files_a, nullptr));
-  ASSERT_FALSE(_spoor_runtime_TraceFilesEqual(nullptr, &trace_files_a));
-  ASSERT_EQ(trace_files_a, trace_files_a);
-  ASSERT_EQ(trace_files_a, trace_files_b);
-  ASSERT_NE(trace_files_a, trace_files_c);
-  ASSERT_NE(trace_files_a, trace_files_d);
-  ASSERT_EQ(trace_files_c, trace_files_d);
-  ASSERT_EQ(malformed_trace_files_e, malformed_trace_files_e);
-  ASSERT_NE(trace_files_a, malformed_trace_files_e);
-  ASSERT_NE(trace_files_a, malformed_trace_files_f);
-  ASSERT_NE(malformed_trace_files_e, malformed_trace_files_f);
-}
-
 TEST(Runtime, DeletedFilesInfoEquality) {  // NOLINT
-  constexpr _spoor_runtime_DeletedFilesInfo info_a{.deleted_files = 1,
-                                                   .deleted_bytes = 2};
-  constexpr _spoor_runtime_DeletedFilesInfo info_b{.deleted_files = 1,
-                                                   .deleted_bytes = 2};
-  constexpr _spoor_runtime_DeletedFilesInfo info_c{.deleted_files = 0,
-                                                   .deleted_bytes = 0};
-  ASSERT_TRUE(_spoor_runtime_DeletedFilesInfoEqual(nullptr, nullptr));
-  ASSERT_FALSE(_spoor_runtime_DeletedFilesInfoEqual(&info_a, nullptr));
-  ASSERT_FALSE(_spoor_runtime_DeletedFilesInfoEqual(nullptr, &info_a));
+  constexpr spoor::runtime::DeletedFilesInfo info_a{
+      .deleted_files = 1,
+      .deleted_bytes = 2,
+  };
+  constexpr spoor::runtime::DeletedFilesInfo info_b{
+      .deleted_files = 1,
+      .deleted_bytes = 2,
+  };
+  constexpr spoor::runtime::DeletedFilesInfo info_c{
+      .deleted_files = 0,
+      .deleted_bytes = 0,
+  };
   ASSERT_EQ(info_a, info_a);
   ASSERT_EQ(info_a, info_b);
   ASSERT_NE(info_a, info_c);
-
-  constexpr spoor::runtime::DeletedFilesInfo info_1{.deleted_files = 1,
-                                                    .deleted_bytes = 2};
-  constexpr spoor::runtime::DeletedFilesInfo info_2{.deleted_files = 1,
-                                                    .deleted_bytes = 2};
-  constexpr spoor::runtime::DeletedFilesInfo info_3{.deleted_files = 0,
-                                                    .deleted_bytes = 0};
-  ASSERT_EQ(info_1, info_1);
-  ASSERT_EQ(info_1, info_2);
-  ASSERT_NE(info_1, info_3);
 }
 
 TEST(Runtime, ConfigEquality) {  // NOLINT
@@ -129,109 +34,7 @@ TEST(Runtime, ConfigEquality) {  // NOLINT
   ASSERT_EQ(path_a, path_c);
   ASSERT_EQ(path_a.size(), path_d.size());
 
-  constexpr _spoor_runtime_Config config_a{
-      .trace_file_path_size = path_a.size(),
-      .trace_file_path = path_a.data(),
-      .session_id = 1,
-      .thread_event_buffer_capacity = 2,
-      .max_reserved_event_buffer_slice_capacity = 3,
-      .max_dynamic_event_buffer_slice_capacity = 4,
-      .reserved_event_pool_capacity = 5,
-      .dynamic_event_pool_capacity = 6,
-      .dynamic_event_slice_borrow_cas_attempts = 7,
-      .event_buffer_retention_duration_nanoseconds = 8,
-      .max_flush_buffer_to_file_attempts = 9,
-      .flush_all_events = true};
-  constexpr _spoor_runtime_Config config_b{
-      .trace_file_path_size = path_a.size(),
-      .trace_file_path = path_a.data(),
-      .session_id = 1,
-      .thread_event_buffer_capacity = 2,
-      .max_reserved_event_buffer_slice_capacity = 3,
-      .max_dynamic_event_buffer_slice_capacity = 4,
-      .reserved_event_pool_capacity = 5,
-      .dynamic_event_pool_capacity = 6,
-      .dynamic_event_slice_borrow_cas_attempts = 7,
-      .event_buffer_retention_duration_nanoseconds = 8,
-      .max_flush_buffer_to_file_attempts = 9,
-      .flush_all_events = true};
-  constexpr _spoor_runtime_Config config_c{
-      .trace_file_path_size = path_c.size(),
-      .trace_file_path = path_c.data(),
-      .session_id = 1,
-      .thread_event_buffer_capacity = 2,
-      .max_reserved_event_buffer_slice_capacity = 3,
-      .max_dynamic_event_buffer_slice_capacity = 4,
-      .reserved_event_pool_capacity = 5,
-      .dynamic_event_pool_capacity = 6,
-      .dynamic_event_slice_borrow_cas_attempts = 7,
-      .event_buffer_retention_duration_nanoseconds = 8,
-      .max_flush_buffer_to_file_attempts = 9,
-      .flush_all_events = true};
-  constexpr _spoor_runtime_Config config_d{
-      .trace_file_path_size = path_d.size(),
-      .trace_file_path = path_d.data(),
-      .session_id = 1,
-      .thread_event_buffer_capacity = 2,
-      .max_reserved_event_buffer_slice_capacity = 3,
-      .max_dynamic_event_buffer_slice_capacity = 4,
-      .reserved_event_pool_capacity = 5,
-      .dynamic_event_pool_capacity = 6,
-      .dynamic_event_slice_borrow_cas_attempts = 7,
-      .event_buffer_retention_duration_nanoseconds = 8,
-      .max_flush_buffer_to_file_attempts = 9,
-      .flush_all_events = true};
-  constexpr _spoor_runtime_Config config_e{
-      .trace_file_path_size = path_a.size(),
-      .trace_file_path = path_a.data(),
-      .session_id = 0,
-      .thread_event_buffer_capacity = 0,
-      .max_reserved_event_buffer_slice_capacity = 0,
-      .max_dynamic_event_buffer_slice_capacity = 0,
-      .reserved_event_pool_capacity = 0,
-      .dynamic_event_pool_capacity = 0,
-      .dynamic_event_slice_borrow_cas_attempts = 0,
-      .event_buffer_retention_duration_nanoseconds = 0,
-      .max_flush_buffer_to_file_attempts = 0,
-      .flush_all_events = false};
-  constexpr _spoor_runtime_Config malformed_config_f{
-      .trace_file_path_size = 1,
-      .trace_file_path = nullptr,
-      .session_id = 1,
-      .thread_event_buffer_capacity = 2,
-      .max_reserved_event_buffer_slice_capacity = 3,
-      .max_dynamic_event_buffer_slice_capacity = 4,
-      .reserved_event_pool_capacity = 5,
-      .dynamic_event_pool_capacity = 6,
-      .dynamic_event_slice_borrow_cas_attempts = 7,
-      .event_buffer_retention_duration_nanoseconds = 8,
-      .max_flush_buffer_to_file_attempts = 9,
-      .flush_all_events = true};
-  constexpr _spoor_runtime_Config malformed_config_g{
-      .trace_file_path_size = 1,
-      .trace_file_path = path_a.data(),
-      .session_id = 1,
-      .thread_event_buffer_capacity = 2,
-      .max_reserved_event_buffer_slice_capacity = 3,
-      .max_dynamic_event_buffer_slice_capacity = 4,
-      .reserved_event_pool_capacity = 5,
-      .dynamic_event_pool_capacity = 6,
-      .dynamic_event_slice_borrow_cas_attempts = 7,
-      .event_buffer_retention_duration_nanoseconds = 8,
-      .max_flush_buffer_to_file_attempts = 9,
-      .flush_all_events = true};
-  ASSERT_TRUE(_spoor_runtime_ConfigEqual(nullptr, nullptr));
-  ASSERT_FALSE(_spoor_runtime_ConfigEqual(&config_a, nullptr));
-  ASSERT_FALSE(_spoor_runtime_ConfigEqual(nullptr, &config_a));
-  ASSERT_EQ(config_a, config_a);
-  ASSERT_EQ(config_a, config_b);
-  ASSERT_EQ(config_a, config_c);
-  ASSERT_NE(config_a, config_d);
-  ASSERT_NE(config_a, config_e);
-  ASSERT_NE(config_a, malformed_config_f);
-  ASSERT_NE(config_a, malformed_config_g);
-
-  const spoor::runtime::Config config_1{
+  const spoor::runtime::Config config_a{
       .trace_file_path = path_a,
       .session_id = 1,
       .thread_event_buffer_capacity = 2,
@@ -242,8 +45,9 @@ TEST(Runtime, ConfigEquality) {  // NOLINT
       .dynamic_event_slice_borrow_cas_attempts = 7,
       .event_buffer_retention_duration_nanoseconds = 8,
       .max_flush_buffer_to_file_attempts = 9,
-      .flush_all_events = true};
-  const spoor::runtime::Config config_2{
+      .flush_all_events = true,
+  };
+  const spoor::runtime::Config config_b{
       .trace_file_path = path_a,
       .session_id = 1,
       .thread_event_buffer_capacity = 2,
@@ -254,8 +58,9 @@ TEST(Runtime, ConfigEquality) {  // NOLINT
       .dynamic_event_slice_borrow_cas_attempts = 7,
       .event_buffer_retention_duration_nanoseconds = 8,
       .max_flush_buffer_to_file_attempts = 9,
-      .flush_all_events = true};
-  const spoor::runtime::Config config_3{
+      .flush_all_events = true,
+  };
+  const spoor::runtime::Config config_c{
       .trace_file_path = path_c,
       .session_id = 1,
       .thread_event_buffer_capacity = 2,
@@ -266,8 +71,9 @@ TEST(Runtime, ConfigEquality) {  // NOLINT
       .dynamic_event_slice_borrow_cas_attempts = 7,
       .event_buffer_retention_duration_nanoseconds = 8,
       .max_flush_buffer_to_file_attempts = 9,
-      .flush_all_events = true};
-  const spoor::runtime::Config config_4{
+      .flush_all_events = true,
+  };
+  const spoor::runtime::Config config_d{
       .trace_file_path = path_d,
       .session_id = 1,
       .thread_event_buffer_capacity = 2,
@@ -278,8 +84,9 @@ TEST(Runtime, ConfigEquality) {  // NOLINT
       .dynamic_event_slice_borrow_cas_attempts = 7,
       .event_buffer_retention_duration_nanoseconds = 8,
       .max_flush_buffer_to_file_attempts = 9,
-      .flush_all_events = true};
-  const spoor::runtime::Config config_5{
+      .flush_all_events = true,
+  };
+  const spoor::runtime::Config config_e{
       .trace_file_path = path_a,
       .session_id = 0,
       .thread_event_buffer_capacity = 0,
@@ -290,12 +97,13 @@ TEST(Runtime, ConfigEquality) {  // NOLINT
       .dynamic_event_slice_borrow_cas_attempts = 0,
       .event_buffer_retention_duration_nanoseconds = 0,
       .max_flush_buffer_to_file_attempts = 0,
-      .flush_all_events = false};
-  ASSERT_EQ(config_1, config_1);
-  ASSERT_EQ(config_1, config_2);
-  ASSERT_EQ(config_1, config_3);
-  ASSERT_NE(config_1, config_4);
-  ASSERT_NE(config_1, config_5);
+      .flush_all_events = false,
+  };
+  ASSERT_EQ(config_a, config_a);
+  ASSERT_EQ(config_a, config_b);
+  ASSERT_EQ(config_a, config_c);
+  ASSERT_NE(config_a, config_d);
+  ASSERT_NE(config_a, config_e);
 }
 
 }  // namespace
--- spoor/runtime/runtime_test.cc
+++ spoor/runtime/runtime_test.cc
@@ -33,21 +33,6 @@ ACTION_P(Notify, notification) {  // NOLINT
 
 TEST(Runtime, Initialize) {  // NOLINT
   for (auto iteration{0}; iteration < 3; ++iteration) {
-    ASSERT_FALSE(_spoor_runtime_Enabled());
-    ASSERT_FALSE(_spoor_runtime_Initialized());
-    _spoor_runtime_Initialize();
-    ASSERT_FALSE(_spoor_runtime_Enabled());
-    ASSERT_TRUE(_spoor_runtime_Initialized());
-    _spoor_runtime_Initialize();
-    ASSERT_FALSE(_spoor_runtime_Enabled());
-    ASSERT_TRUE(_spoor_runtime_Initialized());
-    _spoor_runtime_Deinitialize();
-    ASSERT_FALSE(_spoor_runtime_Enabled());
-    ASSERT_FALSE(_spoor_runtime_Initialized());
-    _spoor_runtime_Deinitialize();
-    ASSERT_FALSE(_spoor_runtime_Enabled());
-    ASSERT_FALSE(_spoor_runtime_Initialized());
-
     ASSERT_FALSE(spoor::runtime::Enabled());
     ASSERT_FALSE(spoor::runtime::Initialized());
     spoor::runtime::Initialize();
@@ -66,24 +51,6 @@ TEST(Runtime, Initialize) {  // NOLINT
 }
 
 TEST(Runtime, Enable) {  // NOLINT
-  ASSERT_FALSE(_spoor_runtime_Enabled());
-  _spoor_runtime_Initialize();
-  ASSERT_FALSE(_spoor_runtime_Enabled());
-  for (auto iteration{0}; iteration < 3; ++iteration) {
-    _spoor_runtime_Enable();
-    ASSERT_TRUE(_spoor_runtime_Enabled());
-    _spoor_runtime_Enable();
-    ASSERT_TRUE(_spoor_runtime_Enabled());
-    _spoor_runtime_Disable();
-    ASSERT_FALSE(_spoor_runtime_Enabled());
-    _spoor_runtime_Disable();
-    ASSERT_FALSE(_spoor_runtime_Enabled());
-  }
-  _spoor_runtime_Enable();
-  ASSERT_TRUE(_spoor_runtime_Enabled());
-  _spoor_runtime_Deinitialize();
-  ASSERT_FALSE(_spoor_runtime_Enabled());
-
   ASSERT_FALSE(spoor::runtime::Enabled());
   spoor::runtime::Initialize();
   ASSERT_FALSE(spoor::runtime::Enabled());
@@ -103,27 +70,7 @@ TEST(Runtime, Enable) {  // NOLINT
   ASSERT_FALSE(spoor::runtime::Enabled());
 }
 
-namespace flush_trace_events_running_test {
-
-// clang-format off NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables, fuchsia-statically-constructed-objects) clang-format on
-std::function<void(void)> callback_{};
-
-TEST_F(RuntimeAutoInitialize, FlushTraceEventsRunningCApi) {  // NOLINT
-  _spoor_runtime_FlushTraceEvents({});
-
-  MockFunction<void(void)> callback{};
-  absl::Notification done{};
-  callback_ = callback.AsStdFunction();
-  EXPECT_CALL(callback, Call()).WillOnce(Notify(&done));
-  _spoor_runtime_FlushTraceEvents([] { callback_(); });
-  const auto success =
-      done.WaitForNotificationWithTimeout(kNotificationTimeout);
-  ASSERT_TRUE(success);
-}
-
-}  // namespace flush_trace_events_running_test
-
-TEST_F(RuntimeAutoInitialize, FlushTraceEventsRunningCcApi) {  // NOLINT
+TEST_F(RuntimeAutoInitialize, FlushTraceEvents) {  // NOLINT
   spoor::runtime::FlushTraceEvents({});
 
   MockFunction<void()> callback{};
@@ -135,46 +82,19 @@ TEST_F(RuntimeAutoInitialize, FlushTraceEventsRunningCcApi) {  // NOLINT
   ASSERT_TRUE(success);
 }
 
-namespace flush_trace_events_not_running_test {
-
-// clang-format off NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables, fuchsia-statically-constructed-objects) clang-format on
-std::function<void(void)> callback_{};
-
 TEST(Runtime, FlushTraceEventsNotRunning) {  // NOLINT
-  {
-    _spoor_runtime_FlushTraceEvents({});
+  spoor::runtime::FlushTraceEvents({});
 
-    MockFunction<void(void)> callback{};
-    absl::Notification done{};
-    callback_ = callback.AsStdFunction();
-    EXPECT_CALL(callback, Call()).WillOnce(Notify(&done));
-    _spoor_runtime_FlushTraceEvents([] { callback_(); });
-    const auto success =
-        done.WaitForNotificationWithTimeout(kNotificationTimeout);
-    ASSERT_TRUE(success);
-  }
-  {
-    spoor::runtime::FlushTraceEvents({});
-
-    MockFunction<void()> callback{};
-    absl::Notification done{};
-    EXPECT_CALL(callback, Call()).WillOnce(Notify(&done));
-    spoor::runtime::FlushTraceEvents(callback.AsStdFunction());
-    const auto success =
-        done.WaitForNotificationWithTimeout(kNotificationTimeout);
-    ASSERT_TRUE(success);
-  }
+  MockFunction<void()> callback{};
+  absl::Notification done{};
+  EXPECT_CALL(callback, Call()).WillOnce(Notify(&done));
+  spoor::runtime::FlushTraceEvents(callback.AsStdFunction());
+  const auto success =
+      done.WaitForNotificationWithTimeout(kNotificationTimeout);
+  ASSERT_TRUE(success);
 }
 
-}  // namespace flush_trace_events_not_running_test
-
 TEST(Runtime, ClearTraceEvents) {  // NOLINT
-  _spoor_runtime_Deinitialize();
-  _spoor_runtime_ClearTraceEvents();
-  _spoor_runtime_Initialize();
-  _spoor_runtime_ClearTraceEvents();
-  _spoor_runtime_Deinitialize();
-
   spoor::runtime::Deinitialize();
   spoor::runtime::ClearTraceEvents();
   spoor::runtime::Initialize();
@@ -182,95 +102,39 @@ TEST(Runtime, ClearTraceEvents) {  // NOLINT
   spoor::runtime::Deinitialize();
 }
 
-namespace flushed_trace_files_test {
-
-// clang-format off NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables, fuchsia-statically-constructed-objects) clang-format on
-std::function<void(_spoor_runtime_TraceFiles)> callback_{};
-
 TEST(Runtime, FlushedTraceFiles) {  // NOLINT
-  {
-    constexpr _spoor_runtime_TraceFiles expected_trace_files{
-        .file_paths_size = 0,
-        .file_path_sizes = nullptr,
-        .file_paths = nullptr};
-    MockFunction<void(_spoor_runtime_TraceFiles)> callback{};
-    absl::Notification done{};
-    callback_ = callback.AsStdFunction();
-    EXPECT_CALL(callback, Call(expected_trace_files)).WillOnce(Notify(&done));
-    _spoor_runtime_FlushedTraceFiles(
-        [](auto trace_files) { callback_(trace_files); });
-    const auto success =
-        done.WaitForNotificationWithTimeout(kNotificationTimeout);
-    ASSERT_TRUE(success);
-  }
-  {
-    const std::vector<std::filesystem::path> expected_trace_files{};
-    MockFunction<void(std::vector<std::filesystem::path>)> callback{};
-    absl::Notification done{};
-    EXPECT_CALL(callback, Call(expected_trace_files)).WillOnce(Notify(&done));
-    spoor::runtime::FlushedTraceFiles(callback.AsStdFunction());
-    const auto success =
-        done.WaitForNotificationWithTimeout(kNotificationTimeout);
-    ASSERT_TRUE(success);
-  }
+  const std::vector<std::filesystem::path> expected_trace_files{};
+  MockFunction<void(std::vector<std::filesystem::path>)> callback{};
+  absl::Notification done{};
+  EXPECT_CALL(callback, Call(expected_trace_files)).WillOnce(Notify(&done));
+  spoor::runtime::FlushedTraceFiles(callback.AsStdFunction());
+  const auto success =
+      done.WaitForNotificationWithTimeout(kNotificationTimeout);
+  ASSERT_TRUE(success);
 }
-
-}  // namespace flushed_trace_files_test
-
-namespace delete_flushed_trace_files_older_than_test {
-
-// clang-format off NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables, fuchsia-statically-constructed-objects) clang-format on
-std::function<void(_spoor_runtime_DeletedFilesInfo)> callback_{};
 
 TEST(Runtime, DeleteFlushedTraceFilesOlderThan) {  // NOLINT
-  {
-    constexpr _spoor_runtime_DeletedFilesInfo expected_deleted_files_info{
-        .deleted_files = 0, .deleted_bytes = 0};
-    MockFunction<void(_spoor_runtime_DeletedFilesInfo)> callback{};
-    absl::Notification done{};
-    callback_ = callback.AsStdFunction();
-    EXPECT_CALL(callback, Call(expected_deleted_files_info))
-        .WillOnce(Notify(&done));
-    _spoor_runtime_DeleteFlushedTraceFilesOlderThan(
-        std::numeric_limits<_spoor_runtime_SystemTimestampSeconds>::max(),
-        [](auto deleted_files_info) { callback_(deleted_files_info); });
-    const auto success =
-        done.WaitForNotificationWithTimeout(kNotificationTimeout);
-    ASSERT_TRUE(success);
-  }
-  {
-    constexpr spoor::runtime::DeletedFilesInfo expected_deleted_files_info{
-        .deleted_files = 0, .deleted_bytes = 0};
-    MockFunction<void(spoor::runtime::DeletedFilesInfo)> callback{};
-    absl::Notification done{};
-    EXPECT_CALL(callback, Call(expected_deleted_files_info))
-        .WillOnce(Notify(&done));
-    spoor::runtime::DeleteFlushedTraceFilesOlderThan(
-        std::numeric_limits<spoor::runtime::SystemTimestampSeconds>::max(),
-        callback.AsStdFunction());
-    const auto success =
-        done.WaitForNotificationWithTimeout(kNotificationTimeout);
-    ASSERT_TRUE(success);
-  }
+  constexpr spoor::runtime::DeletedFilesInfo expected_deleted_files_info{
+      .deleted_files = 0, .deleted_bytes = 0};
+  MockFunction<void(spoor::runtime::DeletedFilesInfo)> callback{};
+  absl::Notification done{};
+  EXPECT_CALL(callback, Call(expected_deleted_files_info))
+      .WillOnce(Notify(&done));
+  spoor::runtime::DeleteFlushedTraceFilesOlderThan(
+      std::numeric_limits<spoor::runtime::SystemTimestampSeconds>::max(),
+      callback.AsStdFunction());
+  const auto success =
+      done.WaitForNotificationWithTimeout(kNotificationTimeout);
+  ASSERT_TRUE(success);
 }
 
-}  // namespace delete_flushed_trace_files_older_than_test
-
 TEST(Runtime, GetConfig) {  // NOLINT
-  {
-    const _spoor_runtime_Config expected_config{};
-    const auto config = _spoor_runtime_GetConfig();
-    ASSERT_NE(config, expected_config);
-  }
-  {
-    const spoor::runtime::Config expected_config{};
-    const auto config = spoor::runtime::GetConfig();
-    ASSERT_NE(config, expected_config);
-  }
+  const spoor::runtime::Config expected_config{};
+  const auto config = spoor::runtime::GetConfig();
+  ASSERT_NE(config, expected_config);
 }
 
 TEST(Runtime, StubImplementation) {  // NOLINT
-  ASSERT_FALSE(_spoor_runtime_StubImplementation());
   ASSERT_FALSE(spoor::runtime::StubImplementation());
 }
 
